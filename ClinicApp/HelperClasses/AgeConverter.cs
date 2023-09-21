ebbd7ef249709682fd2111ae0e2f0192f5ee563d
Artur <ibragimov7arthur@gmail.com>
2023-09-21 20:46:03 +0300

    Добавьте файлы проекта.

---
+++ ClinicApp/HelperClasses/AgeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClinicApp.HelperClasses
+{
+    internal class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var now = DateTime.Now;
+            var age = now.Year - ((DateTime)value).Year;
+            if ((DateTime)value > now.AddYears(-age)) age--;
+            return age;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
